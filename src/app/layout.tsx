1157ec43f7ae58ede12df6f5fad45e16b6e437ab
hey pritesh <priteshdhanad2001@gmail.com>
2024-09-10 12:01:11 +0530

    Initial commit for app

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -3,6 +3,7 @@ import { Inter } from "next/font/google";
 import "./globals.css";
 import NavbarDemo from "./../components/NavbarDemo";
 const inter = Inter({ subsets: ["latin"] });
+import Footer from "./../components/Footer";
 
 export const metadata: Metadata = {
   title: "Create Next App",
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,13 +1,21 @@
-import HeroSection from "@/components/HeroSection"
-import FeaturedSection from './../components/FeaturedSection';
+import HeroSection from "@/components/HeroSection";
+import FeaturedSection from "./../components/FeaturedSection";
 import WhyUs from "@/components/WhyUs";
+import { CourseTestimonials } from "@/components/CourseTestimonials";
+import Webinars from "@/components/Webinars";
+import Instructors from "./../components/Instructors";
+import Footer from "@/components/Footer";
 
 export default function Home() {
   return (
     <>
-      <HeroSection/>
+      <HeroSection />
       <FeaturedSection />
       <WhyUs />
+      <CourseTestimonials />
+      <Webinars />
+      <Instructors />
+      <Footer />
     </>
   );
 }
